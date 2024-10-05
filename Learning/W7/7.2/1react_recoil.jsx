235d1814f0ffabe37a116267bba07c5a31ccd226
Vishnu <vishnu10kumar11@gmail.com>
2024-10-05 22:14:00 +0530

    recoil basic

---
+++ Learning/W7/7.2/1react_recoil.jsx
@@ -0,0 +1,24 @@
+
+// The ContextAPI main focus is on to get rid of propdrilling, not to make re-rendering more efficient. 
+// Instead, the ContextAPI re-renders those component as well, which does not use contextAPI variable, 
+// Hence, use ContextAPI accordingly to it. 
+
+
+// Recoil -  It is a state management library for React that enables efficient, flexible state sharing among 
+// components using atoms (units of state) and selectors (derived state). It simplifies state management by 
+// integrating seamlessly with React’s hooks, promoting fine-grained updates without unnecessary re-renders.
+
+
+// usecase:
+
+// Create folder named store, then atoms, then file-name.jsx --- Conventional way, else just create file-name.jsx
+// in count.jsx file:
+
+import { atom } from "recoil";
+
+export const countAtom = atom({
+    key:"countAtom",
+    default:0
+});
+
+

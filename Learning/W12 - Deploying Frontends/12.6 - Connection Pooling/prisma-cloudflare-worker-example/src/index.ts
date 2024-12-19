1906fe0a31bc3c40eb62fcaef16c237c2f23d473
Vishnu <vishnu10kumar11@gmail.com>
2024-12-20 00:37:38 +0530

    just connected a pg db to cloudflare worker

--- Learning/W12 - Deploying Frontends/12.6 - Connection Pooling/prisma-cloudflare-worker-example/src/index.ts
+++ Learning/W12 - Deploying Frontends/12.6 - Connection Pooling/prisma-cloudflare-worker-example/src/index.ts
@@ -1,18 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request:any, env:any, ctx:any) {
+    const pool = new Pool({ connectionString: env.DATABASE_URL })
+    const adapter = new PrismaPg(pool)
+    const prisma = new PrismaClient({ adapter })
+
+    // Create a new user
+    const newUser = await prisma.user.create({
+      data: {
+        name: "John Doe",
+        email: "johndoe@example.com"
+      }
+    });
+
+    const users = await prisma.user.findMany();
+
+    // Send back the list of users as a JSON response
+    return new Response(JSON.stringify(users), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  },
+}
+

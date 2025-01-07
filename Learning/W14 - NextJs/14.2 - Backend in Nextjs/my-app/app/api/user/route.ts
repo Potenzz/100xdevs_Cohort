28a6cd42d60049260f5b21eb8a4b07339befbbe5
Vishnu <vishnu10kumar11@gmail.com>
2025-01-07 23:23:02 +0530

    nextjs backend 1

---
+++ Learning/W14 - NextJs/14.2 - Backend in Nextjs/my-app/app/api/user/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest){
+    const body = await req.json();
+
+    // should add zod validataion here
+    const user = await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })    
+
+    console.log(body);
+
+    return Response.json({
+        message:"You are logged in!"
+    })
+}

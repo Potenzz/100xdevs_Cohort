68915a6ee2a383fc93fa19563177c1a7803b76b2
Vishnu <vishnu10kumar11@gmail.com>
2025-01-13 20:54:19 +0530

    dockercompose done

---
+++ Learning/W15 - Docker/15.3 - Docker push, compose, exec/week-15-live-2.2/src/db.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+if (!process.env.MONGO_URL) {
+  throw new Error('MONGO_URL environment variable is not defined');
+}
+
+const mongoUrl: string = process.env.MONGO_URL;
+
+// Connect to MongoDB
+mongoose.connect(mongoUrl)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Define a User schema
+interface IUser {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true }
+});
+
+// Create a User model
+export const User = model<IUser>('User', UserSchema);
